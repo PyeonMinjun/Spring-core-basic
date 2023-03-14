f8473045feeb8c3ba6d1ef4a3a1b8730f864ee90
PyeonMinjun <alswns9810@gmail.com>
2023-03-14 17:00:28 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -12,19 +12,34 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+
+    //call AppConfig.memberService
+    //call AppConfig.MemberRepository
+    //call AppConfig.MemberRepository
+    //call AppConfig.orderService
+    //call AppConfig.MemberRepository
+    // 순서는 보장하지 않지만 요렇게  MemberRepository가 3번이 호출이 될 것이다.
+
     @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(MemoryMemberRepository());
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(MemberRepository());
     }
 
     @Bean
-    public static MemoryMemberRepository MemoryMemberRepository() {
+    public MemoryMemberRepository MemberRepository() {
+        System.out.println("call AppConfig.MemberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(MemoryMemberRepository(), discountPolicy());
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
     @Bean
---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,36 @@
+package hello.core.singleton;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.AppConfig;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("MemberRepository", MemberRepository.class);
+
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+        System.out.println(" memberRepository2 = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs((memberRepository));
+        assertThat(orderService.getMemberRepository()).isSameAs((memberRepository));
+    }
+}
